7d343149bb5ea95a1db596a704175035525acce2
vinicius Prado <viniciusprado.prado@gmail.com>
2022-04-13 14:28:15 -0300

    Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,12 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position pro = new Position (3,5);
 		
-		System.out.println(pro);
-
+		Board board =new Board (8, 8);
 	}
 
 }
