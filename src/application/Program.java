61900480203fab00659fb97d88e8e0e8ec2975f6
vinicius Prado <viniciusprado.prado@gmail.com>
2022-04-13 14:03:45 -0300

    projeto chess

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Position pro = new Position (3,5);
+		
+		System.out.println(pro);
+
+	}
+
+}
